8932e4ebfad404ad7c1af2e353e4a5ee45345e10
Donghun Park <donghunp1212@gmail.com>
2020-10-17 16:18:01 +0900

    Create 징검다리.py

---
+++ BinarySearch/징검다리.py
@@ -0,0 +1,33 @@
+'''
+제거한 갯수(cnt)를 기준으로
+n보다 작거나 같으면 left = mid + 1
+n보다 크면 right = mid
+'''
+def solution(distance, rocks, n):
+    ans = 0
+    rocks.sort()
+    
+    left = 1
+    right = distance
+    
+    while(left < right):
+        cnt = 0
+        prev = 0
+        mid = (left + right) // 2
+        
+        for i in range(len(rocks)):
+            if(rocks[i] - prev < mid):
+                cnt += 1
+            else:
+                prev = rocks[i]
+                
+        if (distance - prev) < mid : 
+            cnt += 1
+            
+        if cnt <= n:
+            if mid > ans:
+                ans = mid
+            left = mid + 1
+        else:
+            right = mid
+    return ans
