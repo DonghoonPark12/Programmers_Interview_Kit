97ca66eeea7f7023504c694e8da2ef638bd2f272
Donghun Park <donghunp1212@gmail.com>
2020-10-17 13:55:56 +0900

    Create 입국심사.py

---
+++ BinarySearch/입국심사.py
@@ -0,0 +1,10 @@
+def solution(n, times):
+    left , right = 1, max(times) * n
+    while(left < right):
+        mid = (left + right) // 2
+        if sum([mid // x for x in times]) < n :
+            left = mid + 1
+        else:
+            right = mid
+    
+    return left
