eded0816cf2b2232b8950080dadb18cae16c7b0b
Donghun Park <donghunp1212@gmail.com>
2020-10-21 12:27:06 +0900

    Update 순위.py

--- Graph/순위.py
+++ Graph/순위.py
@@ -17,9 +17,9 @@ def solution(n, results):
     for i in range(1, n + 1):
         for l in wins[i]:
             loses[l].update(loses[i]) # loses[5] 에 다가 loses[2]를 update 해준다.
-
+                                      # l은 i에게 졌고,  (가정에 의해) i를 이긴 사람 한테도 진다.
         for w in loses[i]:
-            wins[w].update(wins[i])
+            wins[w].update(wins[i])   # w는 i에게 이겼고,(가정에 의해) i에게 진 사람 한테도 이긴다.
 
     for i in range(1, n + 1):
         if len(wins[i]) + len(loses[i]) == n - 1:
