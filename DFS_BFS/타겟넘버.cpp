5b0d364c46ab782fbd383d1630a42ce1fe9fa610
Donghun Park <donghunp1212@gmail.com>
2020-10-25 18:27:11 +0900

    Create 타겟넘버.cpp

---
+++ DFS_BFS/타겟넘버.cpp
@@ -0,0 +1,32 @@
+'''
+dfs가 아닌 재귀로 해결
+traget을 목표로 뺐을때와 더했을 때 경우를 나누어서 탐색
+'''
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int ans = 0;
+int sum;
+int id;
+void target_number(vector<int> numbers, int target){
+    if(id == numbers.size()){
+        if(target == 0) {
+            ans++; return;
+        }
+        else{
+            id--; return;
+        }
+    }
+    else
+        id++;
+
+    target_number(numbers, target + numbers[id]);
+    target_number(numbers, target - numbers[id]);
+}
+ 
+int solution(vector<int> numbers, int target) {
+    target_number(numbers, target);
+    return ans;
+}
